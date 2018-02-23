73317eb37a914d657b745a3f9cb033e824078e57
matheusbsilva <matheusbattista@hotmail.com>
2018-02-23 20:50:39 -0300

    [Update] Create model Repository

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Repository(models.Model):
+    name = models.CharField(max_length=50)
+    full_name = models.CharField(max_length=100)
+    description = models.CharField(max_length=300, null=True, blank=True)
+    private = models.BooleanField()
+    html_url = models.URLField()
+
+    def __str__(self):
+        return self.name
